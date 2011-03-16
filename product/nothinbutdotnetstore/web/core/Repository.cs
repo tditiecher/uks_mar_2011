6af6d5d596f9155f9f3e1465eb72aaf9261573aa
Taco <taco@tdev.org>
2011-03-16 14:06:06 +0000

    Pushing new changes

---
+++ product/nothinbutdotnetstore/web/core/Repository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace nothinbutdotnetstore.web.application
+{
+    public interface Repository
+    {
+        IEnumerable<Department> get_main_departments();
+    }
+}
