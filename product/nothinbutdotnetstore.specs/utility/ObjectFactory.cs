be53b8ef0dec4f5c41d0a173eba5e2f3ab97d5ac
Taco <taco@tdev.org>
2011-03-17 20:30:10 +0000

    Pushing new changes

--- product/nothinbutdotnetstore.specs/utility/ObjectFactory.cs
+++ product/nothinbutdotnetstore.specs/utility/ObjectFactory.cs
@@ -1,15 +1,23 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Web;
 
 namespace nothinbutdotnetstore.specs.utility
 {
     public class ObjectFactory
     {
-       public static HttpContext create_http_context()
-       {
-           return new HttpContext(create_request(), create_response());
-       }
+        public static HttpContext create_http_context()
+        {
+            return new HttpContext(create_request(), create_response());
+        }
+
+        public static IEnumerable<Item> create_items<Item>(int number_of_items)
+            where Item : new()
+        {
+            return Enumerable.Range(1, number_of_items).Select(x => new Item());
+        }
 
         static HttpRequest create_request()
         {
@@ -18,7 +26,7 @@ namespace nothinbutdotnetstore.specs.utility
 
         static HttpResponse create_response()
         {
-return new HttpResponse(new StringWriter());
+            return new HttpResponse(new StringWriter());
         }
     }
 }
