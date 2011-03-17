d450680e7a9f6071a470f5f93f47aa29a5fcf701
Taco <taco@tdev.org>
2011-03-17 13:22:16 +0000

    Pushing new changes

---
+++ product/nothinbutdotnetstore.specs/CommandUrlSpecs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+using developwithpassion.specifications.rhino;
+using developwithpassion.specifications.extensions;
+using nothinbutdotnetstore.web.core;
+
+namespace nothinbutdotnetstore.specs
+{
+    public class CommandUrlSpecs
+    {
+        public abstract class concern : Observes
+        {
+        }
+
+        public class when_passing_a_type_of_application_behaviour_to_run : concern
+        {
+            Because b = () =>
+                result = CommandUrl.run<ApplicationBehaviour>();
+
+            It should_return_the_url_for_that_type = () =>
+                result.ShouldEqual("ApplicationBehaviour.uk");
+
+            static string result;
+        }
+    }
+
+}
---
+++ product/nothinbutdotnetstore/web/core/CommandUrl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nothinbutdotnetstore.web.core
+{
+    public class CommandUrl
+    {
+        public static string run<ApplicationBehaviour>()
+        {
+            return String.Format("{0}.uk", typeof(ApplicationBehaviour).Name);
+        }
+    }
+}
